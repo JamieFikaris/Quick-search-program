5fb12ca3fa1d1a92b4e74ba60cfaf403494a9124
JamieFikaris <jfbchilly@hotmail.co.uk>
2016-03-22 16:58:31 +0000

    Update Functions.cs

--- Functions.cs
+++ Functions.cs
@@ -10,62 +10,65 @@ namespace Dads_Program
 {
     class Functions
     {
-        public Functions(string inputFromTB)
+        private string input;
+        public String Input
         {
-            this.input = inputFromTB;
-            int num = createFolder();
-            this.fileNum = num;
+            get{ return this.input; }
+            set
+            {
+                if (value == "")
+                    throw new Exception("Empty input!");
+                else
+                this.input = value;
+            } 
         }
 
-        public List<string> fileNames = new List<string>();
-        public string input;
-        public int fileNum;
-        public List<string> foundStuff = new List<string>();
-
-        public int createFolder()
+        private List<string> fileNames = new List<string>();
+        private List<string> foundStuff = new List<string>();
+        public List<string> MyList
         {
-            string path = System.AppDomain.CurrentDomain.BaseDirectory;
-            string dir = @".\Data";
-            int fileCount = 0;
+            get { return foundStuff; }
+        }
 
-            if (!Directory.Exists(System.IO.Path.Combine(path, dir)))
+        public void createFolder()
+        {
+            string path = System.AppDomain.CurrentDomain.BaseDirectory; //set directory to folder app is in
+            string dir = @".\Data";
+
+            if (!Directory.Exists(System.IO.Path.Combine(path, dir))) //if the data folder doesn't exist in the directory
             {
-                Directory.CreateDirectory(System.IO.Path.Combine(path, dir));
+                Directory.CreateDirectory(System.IO.Path.Combine(path, dir)); //create it
             }
             else
             {
-                fileCount = Directory.GetFiles(@".\Data").Length;
-                foreach (string name in Directory.GetFiles(@".\Data", "*.txt",SearchOption.TopDirectoryOnly))
+                foreach (string name in Directory.GetFiles(@".\Data", "*.txt", SearchOption.TopDirectoryOnly))//get all file names in the data folder
                 {
                     fileNames.Add(name);
                 }
             }
-            return fileCount;
         }
-
-        public void read()
+        public void readAndSearch()
         {
-            foreach (string fn in fileNames)
+            foreach (string fn in fileNames) //for every text file in the data folder
             {
                 TextReader text = new StreamReader(Path.Combine(fn));
                 string currentLine;
-                while ((currentLine = text.ReadLine()) != null)
+                while ((currentLine = text.ReadLine()) != null) //while there is still text in the text file
                 {
-                    if (currentLine.Contains(input))
+                    if (currentLine.Contains(input)) //if the input is in the current line
                     {
-                        foundStuff.Add(currentLine);
+                        foundStuff.Add(currentLine); //add to foundstuff list
                     }
                     else
-                        continue;
+                        continue; //if input is not found go to the next line in the text file
                 }
                 text.Close();
             }
 
-            if (foundStuff.Count == 0)
+            if (foundStuff.Count == 0) //if no lines containing the input is found
             {
                 MessageBox.Show("Input was not found in any of the text files!");
             }
-
         }
     }
 }
